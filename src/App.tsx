5fd8b9da8c9847456cfba206c7fdaef43444dae9
senne-hartland <119686535+senne-hartland@users.noreply.github.com>
2023-03-01 17:17:21 +0100

    setting up app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
 import { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
 
-function App() {
-
-  const Search = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
-    const outputRef = useRef<HTMLLIElement>(null);
-    const [suggestion, setSuggestion] = useState([''])
-
-    const RenderList = () => {
-      return <>{suggestion.map(sugge => <li>{sugge}</li>)}</>
-    }
-
-    return <>
-      <form>
-        <input className="search-section__input" type="search" id="searchText" ref={inputRef}/>
-        <input type="submit" className="button-2 search-button" id="searchButton" value="Search" onClick={() => {
-            const inputVal = inputRef.current?.value || '';
-            setSuggestion(suggestion =>  [...suggestion, inputVal])
-            outputRef.current!.textContent = inputVal || '';
-          }}></input>
-          <ul className="search-section__ul" id="suggestionList">
-            <RenderList />
-          </ul>
-      </form>
-    </>
-  }
 
   const Gallery = () => {
     return <></>
@@ -35,6 +13,44 @@ function App() {
   const Buttons = () => {
     return <></>
   }
+
+function App() {
+  const [history, setHistory] = useState(['cat', 'dog', 'duck', 'catdog', 'seal']);
+
+  const SearchBar = () => {
+    return (
+      <Stack spacing={2} sx={{ width: 300 }}>
+        <Autocomplete
+          id="free-solo-2-demo"
+          disableClearable
+          options={history.map((option) => option)}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Search input"
+              InputProps={{
+                ...params.InputProps,
+                type: 'search',
+              }}
+            />
+          )}
+        />
+      </Stack>
+    );
+  }
+  
+  const Search = () => {  
+    return <>
+      <form className='search'>
+        <section className='search__section'>
+          <ul className="search__section__ul" id="suggestionList">
+            <SearchBar />
+          </ul>
+        </section>
+      </form>
+    </>
+    }
+  
 
   return (
     <div className="App">
